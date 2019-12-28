8416755e7d3279b4fab48f6c554672fef6631f70
Eian Carter <eian@bricleir.com>
2019-12-27 21:35:47 -0800

    orglist added

--- pollmack-app/src/components/Organizations/OrganizationCard.js
+++ pollmack-app/src/components/Organizations/OrganizationCard.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import { Grommet, Box } from 'grommet';
+import { grommet } from 'grommet/themes';
+
+const OrganizationCard = () => {
+    return (
+        <Grommet theme={grommet}>
+            <Box align='center'>
+                Hello 
+            </Box>
+        </Grommet>
+    )
+};
+
+export default OrganizationCard;
--- pollmack-app/src/components/Organizations/OrganizationList.js
+++ pollmack-app/src/components/Organizations/OrganizationList.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import { Grommet, Box, Header, Anchor } from 'grommet';
+import { grommet } from 'grommet/themes'; 
+
+import OrganizationCard from './OrganizationCard';
+
+
+const OrganizationList = () => {
+    return (
+        <Grommet theme={grommet}>
+            <Header background="light-4" pad="small"> pollmack
+                <Box direction="row" gap="medium">
+                    <Anchor label="Home" href="/profile" />
+                </Box>
+                {/* <ProfileAvatar /> */}
+            </Header>
+            <Box>
+                <OrganizationCard />
+            </Box>
+        </Grommet>
+    )
+};
+
+export default OrganizationList;
+ 
