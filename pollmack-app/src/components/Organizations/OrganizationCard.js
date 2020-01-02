3fe88d3afd5ba915e23d34934ba6a95c2cf24b12
Eian Carter <eian@bricleir.com>
2020-01-01 19:52:12 -0800

    pulled fed api for elections

--- pollmack-app/src/components/Organizations/OrganizationCard.js
+++ pollmack-app/src/components/Organizations/OrganizationCard.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { Grommet, Box, Paragraph, Heading } from 'grommet';
+import { Grommet, Box, Paragraph } from 'grommet';
 import { grommet } from 'grommet/themes';
 
-const OrganizationCard = () => {
+ 
+const OrganizationCard = (props) => {
     return (
         <Grommet theme={grommet}>
-            <Heading>Political Organizations</Heading>
-            <Box align='center'>
+            <Box align='center' key={props.id}>
                 <Box border='all' align='center'>
-                    <Heading>Organization Name</Heading>
-                    <Heading>Member Since</Heading>
-                    <Paragraph>Total Donations</Paragraph>    
+                    <Paragraph>Name: {props.name}</Paragraph>
+                    <Paragraph>Party: {props.party}</Paragraph>
+                    <Paragraph>Office: {props.office}</Paragraph>
+                    <Paragraph>District: {props.district}, {props.state}</Paragraph>    
+                    <Paragraph>Status: {props.status}</Paragraph>    
+                    <Paragraph>Organization: {props.organization}</Paragraph> 
                 </Box>
             </Box>
-            <Heading>Social Organizations</Heading>
+            {/* <Heading>Social Organizations</Heading>
             <Box align='center'>
                 <Box border='all' align='center'>
                     <Heading>Organization Name</Heading>
                     <Heading>Member Since</Heading>
                     <Paragraph>Total Donations</Paragraph>
                 </Box>
-            </Box>
+            </Box> */}
         </Grommet>
     )
 };
