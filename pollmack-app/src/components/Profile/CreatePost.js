1a46dc1eab0c5915a97b02cf5f4621607f947489
Eian Carter <eian@bricleir.com>
2020-02-15 15:14:55 -0800

    usereducer to post in profile removed grommet styling

--- pollmack-app/src/components/Profile/CreatePost.js
+++ pollmack-app/src/components/Profile/CreatePost.js
@@ -1,25 +1,69 @@
-import React, { useState } from 'react';
-import { Grommet, TextArea, Button, Box } from 'grommet';
-import { grommet } from 'grommet/themes';
+import React, { useReducer, useRef } from 'react';
 
 
 
-const CreatePost = props => {
-    const [value, setValue] = useState("");
-    const onChange = event => setValue(event.target.value);
-    console.log(onChange);
+const CreatePost = () => {
+    const inputRef = useRef();
+    const [posts, dispatch] = useReducer((state, action) => {
+        switch(action.type){
+            case 'ADD-POST':
+                return [
+                    ...state,
+                    {
+                        id: new Date(),
+                        post: action.post,
+                        like: false
+                    }
+                ]
+            case 'REMOVE-POST':
+                return state.filter((_, index) => index !== action.index);
+            // case 'EDIT-POST':
+            //     return [];
+            case 'LIKE-POST':
+                return !action.like;
+            default: 
+                return state;
+        }
+    }, []);
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'ADD-POST',
+            post: inputRef.current.value
+        })
+        inputRef.current.value = '';
+    }
     return (
-        <Grommet theme={grommet}>
-            <Box align='center'>
-                <TextArea
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
                     align='left' 
-                    value={value} 
-                    onChange={onChange} 
-                    {...props} />
-            </Box>
-            <Button>Post+</Button>
-        </Grommet>
+                    ref={inputRef}
+                />
+                <button>Post+</button>
+            </form>
+            <div>
+                <ul>
+                    {posts.map((item, index) => 
+                        <li
+                            key={item.id}
+                            style={item.like ? {color: 'red'} : null}
+                        >
+                            {item.post}
+                            <button onClick={()=>{dispatch({type:'LIKE-POST', index})}}>
+                                Like
+                            </button>
+                            <button onClick={()=>{dispatch({type:'REMOVE-POST', index})}}>
+                                Remove
+                            </button>
+
+                        </li>
+                    )}
+                </ul>
+            </div>
+
+        </div>
     );
 };
 
--- pollmack-app/src/components/Profile/PostList.js
+++ pollmack-app/src/components/Profile/PostList.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { Grommet, Box, Stack } from 'grommet';
-import { grommet } from 'grommet/themes';
-
 import PostCard from './PostCard';
 import CreatePost from './CreatePost';
 
 
-const PostList = () => {
+const PostList = (props) => {
     return (
-        <Grommet theme={grommet}>
-            <Box
-            width='large'
-            height='medium'
-            justify="center"
-            align="center"
-            pad="large"
-            round="medium"
-            background="linear-gradient(102.77deg, #865ED6 -9.18%, #18BAB9 209.09%)">
-                <CreatePost />
-                <Stack anchor="top-right">
-                    <PostCard />
-                </Stack>
-            </Box>
-        </Grommet>
+        <div>
+            <CreatePost />
+            <PostCard />
+        </div>
     )
 }
 
