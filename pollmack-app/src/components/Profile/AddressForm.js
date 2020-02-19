860f2636487921ce84b433ae8c68249c18d7f253
Eian Carter <eian@bricleir.com>
2020-02-19 08:27:59 -0800

    added modal to address form

--- pollmack-app/src/components/Profile/AddressForm.js
+++ pollmack-app/src/components/Profile/AddressForm.js
@@ -1,41 +1,76 @@
 import React from 'react';
-import { Grommet, Form, FormField, Heading, Button, Box } from 'grommet';
+import { Modal, Button, Form, Col } from 'react-bootstrap';
 
 
-const AddressForm = () => {
+export default function AddressForm(props) {
     return (
-        <Grommet>
-            <Heading>Enter Address</Heading>
-            <Box
-                width='small'
-            >
-                <Form
-                    onSubmit={({ value }) => console.log('Submit: ', value)} size='small'>
-                    <FormField 
-                        label='Address 1'
-                        name='address 1'
-                    />
-                    <FormField
-                        label='Address 2'
-                        name='address 2'
-                    />
-                    <FormField
-                        label='City'
-                        name='city'
-                    />
-                    <FormField
-                        label='State'
-                        name='state'
-                    />
-                    <FormField
-                        label='Zip'
-                        name='ZIP'
-                    />
-                </Form>
-                <Button type='submit' label='Submit' primary={true} />
-            </Box>
-        </Grommet>
-    )
-}
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Address Form
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-export default AddressForm;
+                    <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Group controlId="formGridAddress1">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control placeholder="1234 Main St" />
+                </Form.Group>
+
+                <Form.Group controlId="formGridAddress2">
+                    <Form.Label>Address 2</Form.Label>
+                    <Form.Control placeholder="Apartment, studio, or floor" />
+                </Form.Group>
+
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control as="select">
+                        <option>Choose...</option>
+                        <option>...</option>
+                    </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Group id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
--- pollmack-app/src/components/Profile/AvatarBox.js
+++ pollmack-app/src/components/Profile/AvatarBox.js
@@ -1,25 +1,39 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { Grommet, Box, Heading, Text } from "grommet";
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import AddressForm from './AddressForm';
 import { grommet } from 'grommet/themes';
 import Avatar from './ProfileAvatar';
 
 const AvatarBox = () => {
+  const [address, setAddress] = useState([]);
+  const [addressModalShow, setAddressModalShow] = useState(false);
     return (
       <Grommet theme={grommet}>
         <Box>
             <Box
-                width='medium'
-                height='medium'
-                justify="center"
-                align="center"
-                pad="xlarge"
-                round="medium"
+              style={{boxShadow:"1px 1px 3px grey"}}
+              width='medium'
+              height='medium'
+              justify="center"
+              align="center"
+              pad="xlarge"
+              round="medium"
             >
                 <Avatar size="large" />
                 <Heading>Veronica Mars</Heading>
-                <Text>Member Since: February 2018</Text>
+                <Text>Member Since: <br></br>February 2018</Text>
                 <Text>Total Donated: $389.00</Text>
                 <Text>Candidates Backed: 3</Text>
+                <ButtonToolbar>
+                  <button variant='primary' onClick={()=> setAddressModalShow(true)}>
+                    Address
+                  </button>
+                  <AddressForm 
+                    show={addressModalShow}
+                    onHide={()=> setAddressModalShow(false)}
+                  />
+                </ButtonToolbar>
             </Box>
         </Box>
       </Grommet>
