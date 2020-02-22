a6bdb7230e4180b8258045c8548bb1c7f6592855
Eian Carter <eian@bricleir.com>
2020-02-22 15:35:12 -0800

    changed index in root to process env port

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const server = require("./server");
 
-// const port = process.env.PORT || 5600;
-const port = 5600;
+const port = process.env.PORT || 5600;
 
 server.listen(port, () => {
     console.log(`server is running on localhost:${port}`)
